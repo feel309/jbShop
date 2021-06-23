4b6bf7074c0a3eec2c86380a71e34c67a3040039
feel309 <feel3009@naver.com>
2021-06-23 17:56:46 +0900

    first commit

---
+++ src/main/java/com/practice/jbShop/JbShopApplication.java
@@ -0,0 +1,13 @@
+package com.practice.jbShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/jbShop/JbShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.jbShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
